9080bfc85f42a5e0730029f36917175f40c94228
George <goggeliz@gmail.com>
2022-11-18 19:49:24 +0200

    git check

--- src/main/java/ITMO/secondTaskHW/GitCheck.java
+++ src/main/java/ITMO/secondTaskHW/GitCheck.java
@@ -3,5 +3,6 @@ package ITMO.secondTaskHW;
 public class GitCheck {
     public static void main(String[] args) {
         System.out.println("Git");
+        System.out.println("bad service");
     }
 }
