3bae024ee378750ee9124a387a252d722805eec4
George <goggeliz@gmail.com>
2022-12-10 17:59:08 +0200

    Complete HW 7

---
+++ src/main/java/ITMO/Lab7/Main.java
@@ -0,0 +1,11 @@
+package ITMO.Lab7;
+
+public class Main {
+    public static void main(String[] args) {
+        Plane plane = new Plane();
+        plane.wing.ShowWingWeightMethod(10);
+        Plane plane1 = new Plane();
+        plane1.wing.ShowWingWeightMethod(2);
+
+    }
+}
